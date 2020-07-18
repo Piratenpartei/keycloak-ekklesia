82766eb57104cbdfa7a3ca8a9643b0bed50c35f3
Holger Burbach <plexar@me.com>
2020-07-18 18:38:48 +0200

    1st demo plugin

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,5 @@
-/Users/holgerburbach/Documents/_Piratenpartei/BEO/keycloak-10.0.2/bin/jboss-cli.sh --command="module add --name=org.ekklesiademocracy.auidoidcmapper --resources=target/keycloak-ekklesia-0.0.1-SNAPSHOT.jar --dependencies=org.keycloak.keycloak-core,org.keycloak.keycloak-server-spi"
+#set $KEYCLOAK_HOME to keycloak installation directory
+$KEYCLOAK_HOME/bin/jboss-cli.sh --command="module remove --name=org.ekklesiademocracy.auidoidcmapper" 
+$KEYCLOAK_HOME/bin/jboss-cli.sh --command="module add --name=org.ekklesiademocracy.auidoidcmapper --resources=target/keycloak-ekklesia-0.0.1-SNAPSHOT.jar --dependencies=org.keycloak.keycloak-core,org.keycloak.keycloak-server-spi"
+
+
