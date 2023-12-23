f16848e5b37eee77706ed2ba26333e9156efe9e5
Nico Borgsmüller <me@kaenganxt.dev>
2023-12-23 21:24:08 +0100

    Try with one page enabled

--- keycloak-theme/src/main/resources/theme/keycloak-ekklesia/account/src/app/content/ekklesia-beo-settings/EkklesiaBeoSettings.tsx
+++ keycloak-theme/src/main/resources/theme/keycloak-ekklesia/account/src/app/content/ekklesia-beo-settings/EkklesiaBeoSettings.tsx
@@ -22,7 +22,7 @@ interface NotifySettings {
 
 export class EkklesiaBeoSettings extends React.Component {
     static contextType = AccountServiceContext;
-    //context: React.ContextType<typeof AccountServiceContext>;
+    context: React.ContextType<typeof AccountServiceContext>;
 
     private readonly DEFAULT_STATE: NotifySettings = {
       formFields: {},
@@ -36,25 +36,25 @@ export class EkklesiaBeoSettings extends React.Component {
     }
 
     fetchPersonalInfo() {
-      /*this.context!.doGet<FormFields>("/")
-      .then((response: HttpResponse<FormFields>) => {
-        this.setState(this.DEFAULT_STATE);
-        const formFields = response.data;
-        if (!formFields!.attributes) {
-            formFields!.attributes = { notify_matrix_ids: [] };
-        }
-        if (!formFields!.attributes.notify_matrix_ids) {
-            formFields!.attributes.notify_matrix_ids = [];
-        }
-        let enable_email = formFields!.attributes.notify_enable_email;
-        if (typeof enable_email !== "object" || enable_email.length != 1) {
-          formFields!.attributes.notify_enable_email = true;
-        } else {
-          formFields!.attributes.notify_enable_email = enable_email[0] === "true";
-        }
+      this.context!.doGet<FormFields>("/")
+        .then((response: HttpResponse<FormFields>) => {
+          this.setState(this.DEFAULT_STATE);
+          const formFields = response.data;
+          if (!formFields!.attributes) {
+              formFields!.attributes = { notify_matrix_ids: [] };
+          }
+          if (!formFields!.attributes.notify_matrix_ids) {
+              formFields!.attributes.notify_matrix_ids = [];
+          }
+          let enable_email = formFields!.attributes.notify_enable_email;
+          if (typeof enable_email !== "object" || enable_email.length != 1) {
+            formFields!.attributes.notify_enable_email = true;
+          } else {
+            formFields!.attributes.notify_enable_email = enable_email[0] === "true";
+          }
 
-        this.setState({...{ formFields: formFields }});
-      });*/
+          this.setState({...{ formFields: formFields }});
+        });
     }
 
     private handleChange = (value: string, event: React.FormEvent<HTMLInputElement>) => {
@@ -93,13 +93,13 @@ export class EkklesiaBeoSettings extends React.Component {
       reqData.attributes!.notify_matrix_ids = matrixIds.filter(id => id);
       this.setState({...{ formFields: reqData }});
 
-      /*this.context!.doPost<void>("/", reqData)
+      this.context!.doPost<void>("/", reqData)
           .then(() => {
               ContentAlert.success('accountUpdatedMessage');
           })
           .catch(() => {
               ContentAlert.warning("accountUpdateFailedReload");
-          });*/
+          });
     }
 
     private addMatrix = (): void => {
